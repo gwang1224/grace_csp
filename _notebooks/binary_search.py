e39c977569a7a8cb8dbdef50e96e18760d85bd6c
gwang1224 <gracewang187@gmail.com>
2022-11-18 00:31:47 -0800

    tri 2 lesson

--- _notebooks/binary_search.py
+++ _notebooks/binary_search.py
@@ -37,41 +37,37 @@
 
 import random
 
-# print("Hello! What is your name?")
-# name = input("Name:")
-
 num_guesses = 0
 user_guess = 0
-
-# upper_bound = 0
-# lower_bound = 0
+upper_bound = 100
+lower_bound = 0
 
 number = random.randint(1,100)
 print(number)
 
 print(f"I'm thinking of a number between 1 and 100.")
-end_game = False
 
 def guess():
     guess = int(input("Guess a number: "))
     return guess
 
 def search(number, guess):
+    global lower_bound
+    global upper_bound
     if guess < number:
         print("Your guess was too low")
-        global lower_bound = guess
+        lower_bound = guess
     elif guess > number:
         print("Your guess was too high")
-        global upper_bound = guess
-    else:
-        end_game = True
+        upper_bound = guess
     return lower_bound, upper_bound
 
 while user_guess != number:
     user_guess = guess()
     num_guesses += 1
     print(f"You guessed {user_guess}.")
-    upper_bound, lower_bound = search(number, user_guess)
+    lower_bound, upper_bound = search(number, user_guess)
     print(f"Guess a number between {lower_bound} and {upper_bound}.")
 
 print(f"You guessed the number in {num_guesses} guesses!")
+
